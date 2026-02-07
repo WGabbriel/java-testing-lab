c0ec0c96c7f92c6a5b906e4f800a5d91dda6c175
Wallace Gabriel <gs008963@gmail.com>
2026-02-07 19:22:00 -0300

    Initial Commit

---
+++ src/main/java/com/ifpe/java_testing_lab/JavaTestingLabApplication.java
@@ -0,0 +1,13 @@
+package com.ifpe.java_testing_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestingLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpe/java_testing_lab/JavaTestingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpe.java_testing_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
